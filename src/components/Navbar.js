ba815f8f0b38e725c73e007c50caef66deff83c4
Daniel Hoffmann <103540391+Pepa90210@users.noreply.github.com>
2022-08-11 13:01:16 -0500

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
-import horoscopesObj from '../data/horoscopes';
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+import horoscopesObj from "../data/horoscopes";
+// import Match from './Match'
 
 const Navbar = () => {
   const horoscopes = Object.keys(horoscopesObj);
+  const { setCurrentSign } = useContext(HoroscopeContext);
+
+  // console.log(Match)
+  console.log("Navbar is re-rendering");
 
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span onClick={() => setCurrentSign(sign)} key={sign}>
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
